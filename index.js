80750a36c87cbe143347afe8ea9b87fc8071113f
enkw <iankwolfe@gmail.com>
2024-03-27 18:15:31 -0400

    Continued work on js, writeFile successfully writes logo.svg, ironing out kinks

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const generateShapes = require('./lib/shapes');
 const questions = [
     {
         type: 'input',
-        name: 'title',
+        name: 'text',
         message: 'Please enter a 3 letter name/title for this logo:',
         validate: (input) => {
             if (input.length === 3) {
@@ -33,6 +33,28 @@ const questions = [
       {
         type: 'input',
         name: 'shapeColor',
-        message: 'Please enter a basic color name or hexcode for the text:'
+        message: 'Please enter a basic color name or hexcode for the shape:'
       }
 ];
+
+function init() {
+    inquirer.prompt(questions).then((answers) =>
+    {
+        const svgShape = generateShapes(answers);
+        const svgTemplate = `
+<svg width="300" height="200">
+<text x="200" y="100" fill="${answers.textColor}">${answers.text}</text>
+${svgShape}</svg>`;
+
+        fs.writeFile('./examples/logo.svg', svgTemplate, (err) => 
+        {
+            if (err) {
+                console.error('Error creating logo.svg:', err);
+            } else {
+                console.log('logo.svg has been generated');
+            }
+        });
+    });
+}
+
+init();
